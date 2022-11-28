f67d5b44fa582d550c0f16bb4f6b665e1b2773c2
jnievehz <jimwel.nieves@stu.ocsb.ca>
2022-11-28 14:02:06 -0500

    complete section 2

--- JavaApplication3/src/ProblemSet2_2.java
+++ JavaApplication3/src/ProblemSet2_2.java
@@ -2,13 +2,9 @@ public class ProblemSet2_2 {
     public static void main(String[] args) {
         //Use print statements to create your own beautiful original ASCII art.
         //Use comments to describe what your image is depicting.
-        
-        
-        
-        
-        
-        
-        
-        
-    } 
-}
+    	System.out.println(" __      __ ");
+        System.out.println("  O      O  ");
+        System.out.println("  *  L   * ");
+        System.out.println("    ____   ");
+            }
+    }
